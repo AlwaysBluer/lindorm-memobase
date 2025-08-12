2840a5146cfc77458e8f358505c52f7ebd07e9f3
jiangzhijie <jiangzhijie.jzj@alibaba-inc.com>
2025-08-12 19:42:26 +0800

    update version

--- lindormmemobase/__init__.py
+++ lindormmemobase/__init__.py
@@ -8,7 +8,7 @@ This package provides core functionality for:
 - Storage backends for events and profiles
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .main import LindormMemobase, LindormMemobaseError, ConfigurationError
 from .config import Config
